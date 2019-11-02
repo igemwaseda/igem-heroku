a06a95117375be47ddb5792ddc4c0b96148679f4
ryuji0123 <ryo1011.javelin@gmail.com>
2019-11-02 12:53:30 +0900

    add favicon

--- src/resources/views/head.blade.php
+++ src/resources/views/head.blade.php
@@ -11,6 +11,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="shortcut icon" href="image/igem-waseda_logo.png">
 <link href="{{ asset('css/index.css') }}" rel="stylesheet">
 <title>@yield('title')</title>
 @endsection
